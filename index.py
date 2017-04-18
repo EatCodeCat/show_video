a53c1639497599c1893056f636797327948faa20
liangjian <1738757@qq.com>
2017-04-18 21:04:17 +0800

    111

--- index.py
+++ index.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    
     return render_template('index.html',data={'name':'hello world'})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
