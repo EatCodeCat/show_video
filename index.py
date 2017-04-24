ea952311452ac1dcbbe665ede199e1f5e80535cb
EatCodeCat <17387575@qq.com>
2017-04-24 08:33:49 +0800

    頁面

--- index.py
+++ index.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import make_response
+import uuid
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html',data={'name':'hello world'})
+    resp = make_response(render_template('index.html', data={'name':'hello world'}))
+    userId = request.cookies.get('userId')
+    if userId is None:
+        resp.set_cookie('userId', str(uuid.uuid1()))
+    return resp
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0', debug=True)
