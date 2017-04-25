d6d3ee1a081c65982db85dfffd1824f3843362e9
EatCodeCat <17387575@qq.com>
2017-04-26 01:20:43 +0800

    登录页面

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,10 @@
+
+$(".bottom-nav").find('li[name="'+ location.pathname+'"]').addClass('am-active');
 (function($) {
   'use strict';
 
   $(function() {
+  
    
    $(".bottom-nav li").click(function(){
      $(".bottom-nav li").removeClass('am-active')
